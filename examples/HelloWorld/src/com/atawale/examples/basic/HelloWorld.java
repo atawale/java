4cf82b5471ce6104b36de58a23e339696c5f136a
Prakash Atawale <atawale@gmail.com>
2012-11-02 20:27:43 -0700

    Update1

--- examples/HelloWorld/src/com/atawale/examples/basic/HelloWorld.java
+++ examples/HelloWorld/src/com/atawale/examples/basic/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// print hello workd
-		System.out.println("Hello world");
+		System.out.println("Hello world!");
 
 	}
 
