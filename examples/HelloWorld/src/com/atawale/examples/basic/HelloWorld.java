8ba9557df78d2486bbd1fec416cd09fc89ca92a0
Prakash Atawale <atawale@gmail.com>
2012-11-02 20:25:24 -0700

    Hello example

---
+++ examples/HelloWorld/src/com/atawale/examples/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atawale.examples.basic;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// print hello workd
+		System.out.println("Hello world");
+
+	}
+
+}
